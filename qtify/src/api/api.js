8252ece947277ba10640a1de3301051341e5d32c
Sujith <mswaransujith@gmail.com>
2024-09-29 12:06:02 +0530

    qtify

--- qtify/src/api/api.js
+++ qtify/src/api/api.js
@@ -5,6 +5,7 @@ export const Backendpoint = "https://qtify-backend-labs.crio.do";
 export const fetchTopAlbums = async ()=>{
     try {
         const res=await axios.get(`${Backendpoint}/albums/top`)
+        console.log('TopAlbums',res.data)
         return res.data
     }catch(err){
         console.log("Error in Api",err);
@@ -15,6 +16,8 @@ export const fetchTopAlbums = async ()=>{
 export const fetchNewAlbums = async ()=>{
     try {
         const res=await axios.get(`${Backendpoint}/albums/new`)
+        console.log('fetchNewAlbums',res.data)
+
         return res.data
     }catch(err){
         console.log("Error in Api",err);
@@ -23,6 +26,7 @@ export const fetchNewAlbums = async ()=>{
 export const fetchAllSongs = async ()=>{
     try {
         const res=await axios.get(`${Backendpoint}/songs`)
+        console.log('fetchAllSongs',res.data)
         return res.data
     }catch(err){
         console.log("Error in Api",err);
